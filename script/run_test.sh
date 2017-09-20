a9665b6548bedb884e829f2fa5aacd08528fc434
Axel Vasquez <axelv@squez.com.ar>
2017-09-20 02:31:09 -0300

    fix path

--- script/run_test.sh
+++ script/run_test.sh
@@ -3,7 +3,7 @@
 while true; do
     read -p "Do you want to run test program? (y/n)" yn
     case $yn in
-        [Yy]* ) python2 XssScanner.py -v -u https://xss-game.appspot.com/level1/frame; break;;
+        [Yy]* ) python2 ./XssScanner.py -v -u https://xss-game.appspot.com/level1/frame; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer y (yes) or n (no).";;
     esac
