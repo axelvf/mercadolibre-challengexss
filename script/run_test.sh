972ce0267695d8e1acdec79433c7ebfd4bed9e93
Axel Vasquez <axelv@squez.com.ar>
2017-09-20 11:52:46 -0300

    show run test command

--- script/run_test.sh
+++ script/run_test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 while true; do
-    read -p "Do you want to run test program? (y/n)" yn
+    read -p "Do you want to run test program? (y/n) \n the test will run python2 ./XssScanner.py -v -u https://xss-game.appspot.com/level1/frame" yn
     case $yn in
-        [Yy]* ) python2 ./XssScanner.py -v -u https://xss-game.appspot.com/level1/frame; break;;
+        [Yy]* ) echo "Running python2 ./XssScanner.py -v -u https://xss-game.appspot.com/level1/frame";python2 ./XssScanner.py -v -u https://xss-game.appspot.com/level1/frame; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer y (yes) or n (no).";;
     esac
